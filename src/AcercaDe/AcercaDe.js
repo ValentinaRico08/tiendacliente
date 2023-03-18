f93bcd98bf189c197ae4c958b8e373232323b021
Valentina Taborda <valentina18tabordarico@gmail.com>
2023-03-17 20:42:23 -0500

    Se crea index

---
+++ src/AcercaDe/AcercaDe.js
@@ -0,0 +1,20 @@
+export function AcercaDe(){
+    return(
+        <>
+        <section>
+            <div class="container" my-5>
+                <div class="row justify-content-center">
+                    <div class="col-12 col-md-4">
+                        <img src="https://firebasestorage.googleapis.com/v0/b/tiendasisu-b70ee.appspot.com/o/gato2.png?alt=media&token=da02d956-de75-469c-a45f-841180e02247" alt="Foto" class="img-fluid"></img>
+                    </div>
+                    <div class="col-12 col-md-4 text-center align-self-center">
+                        <h2>Tienda Sisu</h2>
+                        <p>PONGA AQUI LA DESCRIPCIÓN</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+        </>
+        
+    )
+}
---
+++ src/Servicios/Servicios.js
@@ -0,0 +1,23 @@
+import './Servicios.css'
+export function Servicios(){
+    return(
+        <>
+            <section class="container-fluid my-5">
+                <div class ="row text-center justify-content-between">
+                    <div class="col-12 col-md-3 fondo">
+                        <h2>Servicio 1</h2>
+                        <i class="bi bi-credit-card-2-front fs-1"></i>
+                    </div>
+                    <div class="col-12 col-md-3 fondo">
+                        <h2>Servicio 2</h2>
+                        <i class="bi bi-credit-card-2-front fs-1"></i>
+                    </div>
+                    <div class="col-12 col-md-3 fondo">
+                        <h2>Servicio 3</h2>
+                        <i class="bi bi-credit-card-2-front fs-1"></i>
+                    </div>
+                </div>
+            </section>
+        </>
+    )
+}
